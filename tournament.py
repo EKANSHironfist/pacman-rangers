c500e78ec1f0496883726451dddabcd8820c6670
EKANSHironfist <jatinkhanulia@gmail.com>
2025-03-29 13:00:09 +0100

    tournament_mcts.csv shows the results of the macthes that myHeuristic had with myMCTSV2_sorting,myMCTSV2,myMCTSV2_weighted_rollout. Each pair had 30 matches each

--- tournament.py
+++ tournament.py
@@ -1,15 +1,14 @@
 import subprocess
 import re
-import os
 import pandas as pd
 import itertools
 from datetime import datetime
 
 # === CONFIGURATION ===
-AGENTS = ["myMCTSV2_rave"] #   "myHeuristics", "myMCTS", "myMCTSV2"
+AGENTS = ["myMCTS", "myHeuristic", "myMCTSV2_rave"]#,"myMCTSV2", "myMCTSV2_sorting","myMCTSV2_weighted_rollout"]      # you can add your model pair here 
 CAPTURE_SCRIPT = "capture.py"
-OUTPUT_LOG = "tournament_rainbow.csv"
-MATCHES_PER_PAIR = 30 # change that number to 50 if you want to run 100 matches per  model pair
+OUTPUT_LOG = "tournament_results.csv"
+MATCHES_PER_PAIR = 100 # change that number to 50 if you want to run 100 matches per  model pair
 VISUAL = True           # Set to False to run silently
 MATCH_TIMEOUT = 600          # Max seconds per match
 
@@ -18,14 +17,14 @@ results = []
 def run_match(red, blue):
     command = [
         "python", CAPTURE_SCRIPT,
-        "-r", f"models/{red}.py",
-        "-b", f"models/{blue}.py"
+        "-r", f"{red}.py",
+        "-b", f"{blue}.py"
     ]
     if not VISUAL:
         command.append("-q")  # Only add quiet mode if visualization is OFF
 
     try:
-        print(f"▶️ {red} (Red) vs {blue} (Blue)")
+        print(f"{red} (Red) vs {blue} (Blue)")
         output = subprocess.check_output(command, stderr=subprocess.STDOUT, timeout=MATCH_TIMEOUT, universal_newlines=True)
 
         # Try to read the score file written by capture.py
@@ -66,18 +65,12 @@ def run_match(red, blue):
     }
 
 # === RUN MATCHES ===
-for red in ["myMCTSV2_rainbow"]:
-    for blue in AGENTS:
-        for _ in range(MATCHES_PER_PAIR):
-            result = pd.DataFrame([run_match(red, blue)])
-            if not os.path.exists(OUTPUT_LOG):
-                result.to_csv(OUTPUT_LOG)
-            else:
-                df = pd.read_csv(OUTPUT_LOG)
-                df = pd.concat([df, result], ignore_index=True)
-                df.to_csv(OUTPUT_LOG)
+for red, blue in itertools.permutations(AGENTS, 2):
+    for _ in range(MATCHES_PER_PAIR):
+        result = run_match(red, blue)
+        results.append(result)
 
 # === SAVE TO CSV ===
-# df = pd.DataFrame(results)
-# df.to_csv(OUTPUT_LOG, index=False)
-# print(f"\n✅ Tournament complete. Results saved to: {OUTPUT_LOG}")
+df = pd.DataFrame(results)
+df.to_csv(OUTPUT_LOG, index=False)
+print(f"\n Tournament complete. Results saved to: {OUTPUT_LOG}")
